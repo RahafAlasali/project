6425d9d4a592855db0e2fc83d94585c4904e5ea5
RahafAl <rahafalasale20@gmail.com>
2026-05-23 19:31:57 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+<template>
+  <div class="text-3xl font-bold text-red-300 underline">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam porro eius
+    vitae cum odio voluptatem asperiores
+    laudantium velit esse? Repellat reiciendis voluptatibus nam nemo recusandae magni id quod voluptatum molestiae.
+
+  </div>
+</template>
